7165fdf2d7bd7fa6a59351d1e2381c6b296d0616
ef-lab <frouman@imbb.forth.gr>
2020-02-28 08:16:11 +0000

    conf files

--- changename.sh
+++ changename.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-sed -r -i s/at-rp.*/at-rp$1/g /etc/hostname /etc/hostname
-sed -r -i s/at-rp.*/at-rp$1/g /etc/hosts /etc/hosts
-sed -r -i s/at-rp.*/at-rp$1/g /etc/salt/minion /etc/salt/minion
+sed -r -i s/ef-rp.*/ef-rp$1/g /etc/hostname /etc/hostname
+sed -r -i s/ef-rp.*/ef-rp$1/g /etc/hosts /etc/hosts
+sed -r -i s/ef-rp.*/ef-rp$1/g /etc/salt/minion /etc/salt/minion
