00ebcad88560011985afe3b6676b8e58e05ceb8c
mdiamantaki <mar.diamantaki@gmail.com>
2020-03-11 11:35:36 +0200

    modify movies_test for less videos

--- conf/movies_test.py
+++ conf/movies_test.py
@@ -10,8 +10,8 @@ def create_conds(params):
 # define parameters
 global conditions
 
-probe1_params = {'probe': [1], 'movie_name': ['o3bgv6'], 'clip_number': list(range(1, 10))}
-probe2_params = {'probe': [2], 'movie_name': ['o1bgv6'], 'clip_number': list(range(1, 10))}
+probe1_params = {'probe': [1], 'movie_name': ['o3bgv6'], 'clip_number': list(range(1, 3))}
+probe2_params = {'probe': [2], 'movie_name': ['o1bgv6'], 'clip_number': list(range(1, 3))}
 
 probe1_conds = create_conds(probe1_params)
 probe2_conds = create_conds(probe2_params)
